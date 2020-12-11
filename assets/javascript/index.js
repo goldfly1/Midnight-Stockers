2551d578697d39a02f4f09b4380369a3670ccf68
goldfly <goldfly1@gmail.com>
2020-12-11 04:42:36 -0500

    some pseudo code added

---
+++ assets/javascript/index.js
@@ -0,0 +1,18 @@
+/*  open stream/ download file   3 different files/streams */
+/*  open and read file  */
+/*  as you read file act on data   build sorted arrays of picked (buy or sell) stocks  OR display news / tickers    */
+/* end of data close file /stream   */
+/*    user buttons for stock picks  */
+/*    check boxes to pick stocks  */
+/*    ul for stock recommendations  */
+/*    ul for stock list  */
+/*    ul for news  */
+/*    string for scrolling ticker  */
+/*      */
+/*    persistent object or data storage for
+      user info/stock portfolio/watch list
+      server side  */
+/*    track portfolio ups and downs  */
+/*    track watchlist ups and downs  */
+/*      */
+/*      */
