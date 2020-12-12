14718ef79861c64ba64be0f2af0f7d0d335be1ac
goldfly <goldfly1@gmail.com>
2020-12-12 13:42:16 -0500

    integrating server-side js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -19,9 +19,10 @@
 
 
 
-function ticker(symbol,price,change){
+function ticker(symbol,open,last){
 /* Stock ticker every 3 minutes
-it's a string crawling across the div when it gets long enough start slicing off it's nose*/
+it's a string crawling across the div when it gets long enough start clipping off it's nose*/
+
 }
 function picks(symbol,name,weekly){
 /* USE S&P 500 I GUESS
@@ -40,10 +41,12 @@ when viewed individually the stock should have a blurbLine
 obj has ticker name buy/sell/nothing   blurb
 */
 }
+
 function news(line){
 /* news feed every 3=30? minutes
 read lines into object    object into array
-replace on news div every 15 seconds*/
+replace on news div every 15 seconds
+REMEMBER TOAST*/
 
 }
 function client(name){
@@ -58,3 +61,20 @@ acount obj     cash balance   value of portfolio   value of pseudo portfolio
 
 the system is a client and has it's own account with it's own portfolio of ALL THORETICAL trades*/
 }
+
+
+
+function getData(){
+      stockData("hog","10min","30","json")
+      return stockData;
+}
+
+//*End Funcion Declarations*//
+
+
+//**get data for stocks */
+
+
+console.log(getData());
+
+
